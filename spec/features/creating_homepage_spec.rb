7027ac3e02d9ccc89df03e24048d1506b01a2fd3
vbaker007 <vbaker007@hotmail.com>
2016-01-19 22:24:37 -0500

    Create File: Creating_Homepage_Spec.rb

---
+++ spec/features/creating_homepage_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.feature "Creating Home Page" do
+  
+  scenario do
+    visit '/'
+
+    expect(page).to have_link('Atheletes Den')
+    expect(page).to have_link('Home')
+    expect(page).to have_content('Workout Lounge')
+  end
+end
