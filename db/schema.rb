eb20b5487873dda16976ac57af43286d9f5da5ed
vbaker007 <vbaker007@hotmail.com>
2016-01-21 04:22:07 -0500

    Create new file: views/exercises/new.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160121014510) do
+ActiveRecord::Schema.define(version: 20160121090634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "exercises", force: :cascade do |t|
+    t.integer  "duration_in_min"
+    t.text     "workout"
+    t.date     "workout_date"
+    t.integer  "user_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "exercises", ["user_id"], name: "index_exercises_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -34,4 +45,5 @@ ActiveRecord::Schema.define(version: 20160121014510) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "exercises", "users"
 end
