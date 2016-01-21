b0770a18fa26bb979529f6f8fe7ff174ca66ea9d
vbaker007 <vbaker007@hotmail.com>
2016-01-21 04:58:51 -0500

    Modify Exercises Controller

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,10 +1,30 @@
 class ExercisesController < ApplicationController
+  before_action :set_exercise, except: [:index, :new, :create]
 
   def index
     @exercises = Exercise.all
   end
 
   def new
-    @exercise = current_user.exercise.new
+    @exercise = current_user.exercises.new
   end
+
+  def create
+    @exercise = current_user.exercise.new(exercise_params)
+
+    if @exercise.save
+      flash[:success] = "Exercise has been created"
+      redirect_to (current_user, @exercise)
+
+    else
+      flash[:danger] = "Exercise has not been created"
+      render :new
+    end
+  end
+
+  private
+
+    def exercise_params
+      params.require(:exercise).permit(:duration_in_min, :workout, :workout_date, :user_id)
+    end
 end
