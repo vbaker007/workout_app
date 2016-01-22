203bc6ce9787299db1cc2b1b30e4491c8523343c
vbaker007 <vbaker007@hotmail.com>
2016-01-21 20:48:32 -0500

    Modify creating_homepage_spec.rb file

--- spec/features/creating_homepage_spec.rb
+++ spec/features/creating_homepage_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "Creating Home Page" do
   scenario do
     visit '/'
 
-    expect(page).to have_link('Atheletes Den')
+    expect(page).to have_link('Athletes Den')
     expect(page).to have_link('Home')
     expect(page).to have_content('Workout Lounge')
   end
