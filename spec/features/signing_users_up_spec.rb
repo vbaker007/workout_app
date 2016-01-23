12e7fe91473fe9f36b20e845233fb0ed2b6fd55c
vbaker007 <vbaker007@hotmail.com>
2016-01-22 22:54:05 -0500

    Modify Specs in signing_users_up_spec.rb file

--- spec/features/signing_users_up_spec.rb
+++ spec/features/signing_users_up_spec.rb
@@ -5,6 +5,8 @@ RSpec.feature "Users signup" do
     visit "/"
 
     click_link "Sign up"
+    fill_in "First name", with: "John"
+    fill_in "Last name", with: "Doe"
     fill_in "Email", with: "user@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
